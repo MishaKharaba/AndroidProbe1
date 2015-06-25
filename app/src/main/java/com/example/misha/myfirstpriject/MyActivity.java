17e5bde06cfef0d01ddd126211f8d84999874c8a
misha.kharaba <misha.kharaba@gmail.com>
2015-06-26 00:10:50 +0300

    Added new activity

--- app/src/main/java/com/example/misha/myfirstpriject/MyActivity.java
+++ app/src/main/java/com/example/misha/myfirstpriject/MyActivity.java
@@ -42,7 +42,7 @@ public class MyActivity extends ActionBarActivity {
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText)findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
