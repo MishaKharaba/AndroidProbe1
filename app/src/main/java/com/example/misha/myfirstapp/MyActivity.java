2405db0373cb5677d561be1da97a94be2bcfd99b
Mykhaylo Kharaba <myhaylo.kharaba@eleks.com>
2015-06-26 18:48:00 +0300

    Shown custom display message

--- app/src/main/java/com/example/misha/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/misha/myfirstapp/MyActivity.java
@@ -1,12 +1,16 @@
 package com.example.misha.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.example.misha.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +38,13 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
